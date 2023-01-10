ea04e7de9427c10ce0d40ed193231abd85f9978d
oricardos <ricardo.rs95@gmail.com>
2023-01-09 23:19:25 -0300

    add ordenacao

--- songs/views.py
+++ songs/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .models import Song
 
 def index(request):
-    songs_datas = Song.objects.filter(is_posted=True)
+    songs_datas = Song.objects.order_by('-song_name').filter(is_posted=True)
 
     songs = {
         'songs': songs_datas
